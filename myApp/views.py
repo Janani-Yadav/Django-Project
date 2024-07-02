48a0ad7b584e2e25c30766b4f9dc4ca2a98813c5
Janani Yadav <janujanani877@gmail.com>
2024-07-02 15:05:42 +0530

    First Version of Django Project

---
+++ myApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from myApp.forms import StudentForm
+# Create your views here.
+
+def formView(request):
+	f=StudentForm() #before submitting the details through form
+	if request.method=='POST':
+		f=StudentForm(request.POST)
+		if f.is_valid():
+			sid=f.cleaned_data["sid"]
+			sname=f.cleaned_data['sname']
+			smarks=f.cleaned_data['smarks']
+			splace=f.cleaned_data['splace']
+			d={'id1':sid,'name':sname,'marks':smarks,'place':splace}
+			return render(request,"myApp/output.html",d)
+	d={'form':f}
+	return render(request,'myApp/input.html',d)
